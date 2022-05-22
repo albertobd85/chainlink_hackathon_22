709b523167cd5f04e2cbfe33000c7f7b4c1bfa57
albertobd85 <albertobd85@gmail.com>
2022-05-22 14:33:00 +0200

    commit

---
+++ foundry-testing/test/StakeContract.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "ds-test/test.sol";
+import "../StakeContract.sol";
+
+contract StakeContractTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+
+    // tokens can be sent to the stake contract
+}
